bd6e580da38d1394473c655a34f8d277ebebdfd9
Patricio Rojas Ortiz <patricio-rojaso@outlook.com>
2019-09-09 21:37:11 -0300

    new version using mysql_config_editor of Package MySQL

--- mysqlbackups.pl
+++ mysqlbackups.pl
@@ -6,9 +6,13 @@
 #|           basename-date-time.sql. You must add it to the cron schema of|
 #|           your server.                                                 |
 #| Platform: Linux                                                        |
+#| Previous: Set your local password with the following command           |
+#| mysql_config_editor set --login-path=local --host=localhost \          |
+#|                                  --user=username --password            |
+#|                                                                        | 
 #| Packages: mysql-server. No additional packages/libraries are required  |
-#| Revision: 2017-04-23 17:22:40                                          |
-#| Version : 2017-01-25 13:58:55                                          |
+#| Revision: 2019-04-17 19:56:33                                          |
+#| Version : 2019-04-17 19:56:33                                          |
 #+------------------------------------------------------------------------+
 #| This source file is subject to the The MIT License (MIT)               |
 #| that is bundled with this package in the file LICENSE.txt.             |
@@ -18,58 +22,77 @@
 #| to patricio-rojaso@outlook.com so we can send you a copy immediately.  |
 #+------------------------------------------------------------------------+
 
-use constant CONS_VERSION  => '2017-01-25 13:58:55';
+use constant CONS_VERSION     => '2019-04-17 19:56:33';
 
 # You must customize the following lines according to your installation
-use constant CONS_PATH_TMP =>'/tmp';
-use constant CONS_USER =>'root';
-use constant CONS_PASS =>'';
-use constant CONS_PATH_OUTPUT=>'/backups/backups/mysql/';
+use constant CONS_PATH_TMP    => '/tmp';
+use constant CONS_PATH_OUTPUT => '/home/pro/repository/mysql/';
 
 use strict;
 use warnings;
 use diagnostics;
 
 my ( $sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst );
-my ( @a_databases, $sl_command, $sl_databases, $tmp );
+my ( @a_databases, $dir_tmp , $command, $databases, $tmp );
 
-	&sub_copyright();
+  &sub_copyright();
 
-	($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
-	$year = $year+1900;
-	$mon  = $mon + 1;
+  ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+  $year = $year+1900;
+  $mon  = $mon + 1;
 
-	chdir( CONS_PATH_TMP ) or die "$!";
+  if (defined $ARGV[0]) {
+    $databases = $ARGV[0];
+  } else {
+    $databases = CONS_PATH_OUTPUT;
+  }
 
-  $sl_command	= "/bin/echo \"SELECT SCHEMA_NAME FROM information_schema.SCHEMATA\" > sql.sql" ;
+  print "Backup to: ", $databases, "\n\n";
 
-	system($sl_command) == 0 or warn "system $sl_command failed: $?";
+  $command    = "/bin/echo \"SELECT SCHEMA_NAME FROM information_schema.SCHEMATA\" > ".CONS_PATH_TMP."/sql.sql  2>&1" ;
+  system($command) == 0 or warn "system $command failed: $?";
 
-	$sl_command	= "/usr/bin/mysql -u" .CONS_USER ." -p".CONS_PASS." < sql.sql > databases.txt" ;
-	system($sl_command) == 0 or warn "system $sl_command failed: $?";
+  $command    = "/usr/bin/mysql --login-path=local < ".CONS_PATH_TMP."/sql.sql > ".CONS_PATH_TMP."/databases.txt  2>&1" ;
+  system($command) == 0 or warn "system $command failed: $?";
 
-	$sl_databases = &sub_file_load( CONS_PATH_TMP. "/databases.txt" );
-	@a_databases  = split( "\n", $sl_databases );
+  $dir_tmp = CONS_PATH_TMP ."/". sprintf("%04d%02d%02d-%02d%02d%02d",  $year, $mon, $mday, $hour, $min, $sec);
 
-	foreach  $tmp ( @a_databases ){
+  $command = "mkdir -p ". $dir_tmp;
+  system($command) == 0 or warn "system $command failed: $?";
 
-		if( length( $tmp )>0){
+  $tmp = &sub_file_load( CONS_PATH_TMP. "/databases.txt" );
+  @a_databases  = split( "\n", $tmp );
 
-			if ($tmp  !~ /SCHEMA_NAME|information_schema|sys|performance_schema|mysql/){
-				print "backups of database:($tmp)\n";
-				$sl_databases = CONS_PATH_OUTPUT . sprintf("%04d%02d%02d-%02d%02d%2d-%s",  $year, $mon, $mday, $hour, $min, $sec, $tmp);
-		    $sl_command 	= "/usr/bin/mysqldump -u ".CONS_USER . " -p" .CONS_PASS ." -B $tmp --single-transaction --flush-logs >".$sl_databases. ".sql";
-				system($sl_command)== 0 or warn "system $sl_command failed: $?";
-			}
-		}
-	}
-	printf "done\n";
-	exit;
+  foreach  $tmp ( @a_databases ) {
+        if ( length( $tmp )>0) {
+            if ($tmp  !~ /SCHEMA_NAME|information_schema|sys|performance_schema|mysql/) {
+                print "backups of database:($tmp)\n";
+                $command   = "/usr/bin/mysqldump --login-path=local -B $tmp --single-transaction --flush-logs >" .$dir_tmp. "/$tmp.sql 2>&1";
+                system($command)== 0 or warn "system $command failed: $?";
+            }
+        }
+  }
+
+  $databases = $databases . sprintf("%04d%02d%02d-%02d%02d%02d",  $year, $mon, $mday, $hour, $min, $sec);
+  $command   = "tar zcf $databases.tar.gz $dir_tmp  2>&1";
+  system($command) == 0 or warn "system $command failed: $?";
+
+  $command   = "rm -rf $dir_tmp  2>&1";
+  system($command) == 0 or warn "system $command failed: $?";
+
+	$command   = "rm -rf ".CONS_PATH_TMP."/sql.sql  2>&1";
+	system($command) == 0 or warn "system $command failed: $?";
+
+	$command   = "rm -rf ".CONS_PATH_TMP."/databases.txt  2>&1";
+	system($command) == 0 or warn "system $command failed: $?";
+
+  printf "done\n";
+  exit;
 
 # ------------------------------------------------------------------------------
 sub sub_copyright(){
 
-	print "\nmysqlbackups.pl V".CONS_VERSION.", Copyright 2016\n\n";
+    print "\nmysqlbackups.pl V".CONS_VERSION.", Copyright 2016\n\n";
 
 }
 
